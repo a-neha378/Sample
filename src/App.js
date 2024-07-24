0723b11633012cc65a8495be273ef90021067654
a-neha378 <neha.agarwal378@gmail.com>
2024-07-24 12:33:09 +0530

    new hook useAction state

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import GitTest from './GitTest';
 import Todo from './components/Todo';
-import Login from './components/Login/Login';
+// import Login from './components/Login/Login';
+import { StatefulForm } from './components/StatefulForm';
 
 function App() {
   return (
     <div className="App">
-      <GitTest />
+       {/* <GitTest />
       <Todo />
-      <Login />
+      <Login /> */}
+      <StatefulForm />
     </div>
   );
 }
